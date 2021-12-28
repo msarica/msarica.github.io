653f13bca8f1c6f10495e4333e327bd4ed99f496
Mehmet Sarica <mehmet.sarica@gmail.com>
2021-12-28 03:01:57 -0500

    fam"

---
+++ sarica/data.js
@@ -0,0 +1,355 @@
+const description = ``;
+
+const dedem = {
+    name: 'Mehmet Sarica',
+    dob: '1928',
+    dod: '2014',
+    description,
+};
+
+const babaannem = {
+    name: 'Naile Sarica',
+    dob: '1928',
+    dod: '1970',
+    description,
+};
+
+const emetullah = {
+    name: 'Emetullah Sarica',
+    dob: '',
+    dod: '',
+    description,
+};
+
+const osman_dag = {
+    name: 'Osman Dag',
+    dob: '',
+    dod: '',
+    description,
+};
+
+const kadriye_dag = {
+    name: 'Kadriye Dag',
+    image: 'kadriye',
+    dob: '',
+    dod: '',
+    description,
+};
+
+const fatih_demirkollu = {
+    name: 'Fatih Demirkoullu',
+    image: '',
+    dob: '',
+    dod: '',
+    description,
+};
+
+const hakan_demirkollu = {
+    name: 'Hakan Demirkoullu',
+    image: '',
+    dob: '',
+    dod: '',
+    description,
+};
+
+const osman_demirkollu = {
+    name: 'Ali Osman Demirkoullu',
+    image: '',
+    dob: '',
+    dod: '',
+    description,
+};
+
+const yasin_dag = {
+    name: 'Yasin Dag',
+    dob: '',
+    dod: '',
+    description,
+};
+
+const bedriye_dag = {
+    name: 'Bedriye Dag',
+    dob: '',
+    dod: '',
+    description,
+};
+
+const rukiye_dag = {
+    name: 'Rukiye Dag',
+    dob: '',
+    dod: '',
+    description,
+};
+
+const mustafa_ulualan = {
+    name: 'Mustafa Ulualan',
+    dob: '',
+    dod: '',
+    description,
+};
+
+const sevdesena = {
+    name: 'Sevde Sena',
+    dob: '',
+    dod: '',
+    description,
+};
+
+const hava = {
+    name: 'Havva Sarica',
+    dob: '',
+    dod: '',
+    description,
+};
+
+const yunus_bayhan = {
+    name: 'Yunus Bayhan',
+    dob: '',
+    dod: '',
+    description,
+};
+
+const halime_bayhan = {
+    name: 'Halime Bayhan',
+    dob: '',
+    dod: '',
+    description,
+};
+
+const rahime_bayhan = {
+    name: 'Rahime Bayhan',
+    dob: '',
+    dod: '',
+    description,
+};
+
+const salih_bayhan = {
+    name: 'Salih Bayhan',
+    dob: '',
+    dod: '',
+    description,
+};
+
+const kubra__ = {
+    name: 'Kubra Ciftci',
+    dob: '',
+    dod: '',
+    description,
+};
+
+const yunus_bayhan_jr = {
+    name: 'Yunus Bayhan',
+    dob: '',
+    dod: '',
+    description,
+};
+
+const ahsen_bayhan_jr = {
+    name: 'Ahsen Bayhan',
+    dob: '',
+    dod: '',
+    description,
+};
+
+
+
+const fatma = {
+    name: 'Fatma Sarica',
+    dob: '',
+    dod: '',
+    description,
+};
+
+const babam = {
+    name: 'Mustafa Sarica',
+    image: 'babam',
+    dob: '1964',
+    dod: '',
+    description,
+};
+
+const hatice = {
+    name: 'Hatice Sarica',
+    dob: '',
+    dod: '',
+    description,
+};
+
+const annem = {
+    name: 'Emine Bayhan',
+    image: 'annem',
+    dob: '1964',
+    dod: '',
+    description,
+};
+
+const ben = {
+    name: 'Mehmet Sarica',
+    image: 'mehmet',
+    dob: '',
+    dod: '',
+    description,
+};
+
+const zehra = {
+    name: 'Zehra Sarica',
+    image: 'zehra',
+    dob: '',
+    dod: '',
+    description,
+};
+
+const omer_kaplan = {
+    name: 'Omer Kaplan',
+    dob: '',
+    dod: '',
+    description,
+};
+
+const mirza_kaplan = {
+    name: 'Mirza Kaplan',
+    image: 'mirza',
+    dob: '',
+    dod: '',
+    description,
+};
+
+const eslem = {
+    name: 'Eslem Sarica',
+    image: 'eslem',
+    dob: '',
+    dod: '',
+    description,
+};
+
+const mesud = {
+    name: 'Mesud Sarica',
+    image: 'mesud',
+    dob: '',
+    dod: '',
+    description,
+};
+
+const mustafa = {
+    name: 'Mustafa Sarica',
+    image: 'mustafa',
+    dob: '',
+    dod: '',
+    description,
+};
+
+const sefa = {
+    name: 'Sefa Demirkollu',
+    image: 'sefa',
+    dob: '',
+    dod: '',
+    description,
+};
+
+var familyData = {
+    person: dedem,
+    marriedTo: babaannem,
+    children: [
+        {
+            person: emetullah,
+            marriedTo: osman_dag,
+            children: [
+                {
+                    person: kadriye_dag,
+                    marriedTo: fatih_demirkollu,
+                    children: [
+                        {
+                            person: sefa,
+                            marriedTo: ben
+                        },
+                        {
+                            person: hakan_demirkollu,
+                            marriedTo: 'gul',
+                            children: [
+                                'metehan demirkollu'
+                            ]
+                        }, {
+                            person: osman_demirkollu
+                        }
+                    ]
+                },
+                {
+                    person: yasin_dag,
+                    marriedTo: 'mukerrem',
+                    children: [
+                        'sedanur',
+                        'sevde'
+                    ]
+                },
+                {
+                    person: bedriye_dag,
+                },
+                {
+                    person: rukiye_dag,
+                    marriedTo: mustafa_ulualan,
+                    children: [
+                        {
+                            person: sevdesena
+                        }
+                    ]
+                },
+            ],
+        },
+        {
+            person: hava,
+            marriedTo: yunus_bayhan,
+            children: [
+                {
+                    person: halime_bayhan
+                },
+                {
+                    person: rahime_bayhan
+                },
+                {
+                    person: salih_bayhan,
+                    marriedTo: kubra__,
+                    children: [
+                        { person: yunus_bayhan_jr },
+                        { person: ahsen_bayhan_jr }
+                    ]
+                }
+            ]
+        },
+        {
+            person: fatma,
+        },
+        {
+            person: babam,
+            marriedTo: annem,
+            children: [
+                {
+                    person: ben,
+                    marriedTo: sefa,
+                    children: [
+                        {
+                            person: mesud,
+                        },
+                        {
+                            person: mustafa,
+                        },
+                    ],
+                },
+                {
+                    person: zehra,
+                    marriedTo: omer_kaplan,
+                    children: [
+                        {
+                            person: mirza_kaplan
+                        }
+                    ]
+                },
+                {
+                    person: eslem,
+                },
+            ],
+        },
+        {
+            person: hatice,
+        },
+    ],
+};
