1abe95eca23cbc8fcc37092e6dc7223dce51e5b7
msarica <mehmet.sarica@gmail.com>
2025-10-06 02:14:30 +0000

    Deploying to main from @ msarica/snake_color_game@0a10c9d752a65167ddbe6b4cbc3115961aec439f 🚀

--- snake_color_puzzle/registerSW.js
+++ snake_color_puzzle/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/snake_color_puzzle/sw.js', { scope: '/snake_color_puzzle/' })})}
